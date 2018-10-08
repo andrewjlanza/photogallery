abfc338902dd68d09f81bd7e305f7ebfc3e1d83d
Andrew Lanza <andrewlanza@Andrews-MBP-2.localdomain>
2018-10-08 10:14:59 -0400

    here we are

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App"></div>
+        <header>  -
+          <nav> -
+          
+            <ul>
+              <li>Home</li>
+            </ul>
+          
+          <main>
+            <aside className="Teams">Teams
+              <p>
+                These are the heavyweight teams that are comprised of the most talent soccer players in the world
+              </p><img href="https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjL88mjg_fdAhWH2FMKHcBnA7UQjRx6BAgBEAU&url=https%3A%2F%2Fprojects.fivethirtyeight.com%2Fglobal-club-soccer-rankings%2F&psig=AOvVaw0pAPU1TkiDP837EwZSOWZZ&ust=1539094269732254"></img> </aside>
+            <aside className="Players">Players</aside>
+          </main>
+          </div>
+       
+      
+
+export default App;
